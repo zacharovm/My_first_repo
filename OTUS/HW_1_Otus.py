b71b356b32291a7d49497ef07b61140e34710fc8
zacharovm <zacharovm@mail.ru>
2024-09-13 19:07:07 +0300

    Add files via upload

--- OTUS/HW_1_Otus.py
+++ OTUS/HW_1_Otus.py
@@ -5,6 +5,7 @@ b = a[::-1]
 c = test_digit[0] + b + test_digit[-1]
 print(c)
 
+
 #2
 days = int(input())
 full_weeks = days // 7
@@ -17,8 +18,17 @@ else:
     target_weekends = full_weeks * 2
 print(target_weekends)
 
-#3
 
+#3
+a = int(input())
+b = int(input())
+c = int(input())
+if c % a != 0:
+    print(False)
+elif c > a*b:
+    print(False)
+else:
+    print(True)
 
 
 #4
@@ -32,3 +42,12 @@ h = hunds[data_to_rome // 100 % 10]
 te = tens[data_to_rome // 10 % 10]
 o = ones[data_to_rome % 10]
 print(t+h+te+o)
+
+
+#5
+a = input()
+try:
+    if float(a) > 0:
+        print(True)
+except Exception:
+    print(False)
